67c0d5534c5f83970bfc092864aab881c594ae8c
Gautami-07 <ami@cloudkarya.com>
2024-06-27 13:02:29 +0530

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
        name='load-csv-to-postgres',
+       version='1.0.0',
        packages=find_packages(),
        install_requires=[
            'apache-beam[gcp]',
