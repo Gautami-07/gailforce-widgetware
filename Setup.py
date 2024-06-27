610daca7191f76c9cf542d52a7fc96b7fa44455c
Gautami-07 <ami@cloudkarya.com>
2024-06-27 15:26:47 +0530

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
        name='load-csv-to-postgres',
-       version='1.0.0',
+       version='2.0.0',
        packages=find_packages(),
        install_requires=[
            'apache-beam[gcp]',
-           'google-cloud-sql-python-connector',
+           'cloud-sql-python-connector["pg8000"]',
            'sqlalchemy',
        ],
 )
